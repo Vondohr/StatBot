00f8c516455429666f0a3040dbbfa5320634c105
Mark Vendohr <vondrr@gmail.com>
2025-06-20 12:35:41 +0200

    More changes

--- cogs/embedManagement.py
+++ cogs/embedManagement.py
@@ -41,13 +41,11 @@ class EmbedCreator(commands.Cog):
         await interaction.response.send_message("Embed posted!", ephemeral=True)
         await interaction.channel.send(embed=embed)
 
-        dm_message = (
-                    "Your Bounty Hunter character has been approved! Please change your nickname on the server to.\n\n"
+        dm_message = ("Your Bounty Hunter character has been approved!\n\n"
+                    
+                    f"1. Please change your nickname on the server to \"{title}\".\n\n"
 
-                    "1. [Watch our Opening Crawl on Youtube!](https://www.youtube.com/watch?v=TVDm77NpEXI)\n\n"
-
-                    "2. Head over to the [Purchasing Guide](https://discord.com/channels/1258379465919041589/1276474859018391615) to continue getting your Character ready.\n\n"
-                )
+                    "2. Head over to the [Purchasing Guide](https://discord.com/channels/1258379465919041589/1276474859018391615) to continue getting your Character ready.")
 
         await member.send(dm_message)
 

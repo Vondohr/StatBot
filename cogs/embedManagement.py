85823f3fe12503525c91c52437af077f81e1f303
Mark Vendohr <vondrr@gmail.com>
2025-06-20 12:13:19 +0200

    testing new functionality

--- cogs/embedManagement.py
+++ cogs/embedManagement.py
@@ -41,6 +41,13 @@ class EmbedCreator(commands.Cog):
         await interaction.response.send_message("Embed posted!", ephemeral=True)
         await interaction.channel.send(embed=embed)
 
+        dm_message = (
+                    "Your Bounty Hunter character has been approved! Please change your nickname on the server to."
+                    "Head over to the [Purchasing Guide](https://discord.com/channels/1258379465919041589/1276474859018391615) to continue getting your Character ready."
+                )
+
+        await member.send(dm_message)
+
     @app_commands.command(name="admin_embed_edit", description="Edit an existing embed message")
     @app_commands.describe(
         channel="Channel where the message is located",

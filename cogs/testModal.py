5cc1d669f94a675f86af5b860c81bcf27c7517e0
Mark Vendohr <vondrr@gmail.com>
2025-10-12 21:05:42 +0200

    Testing some more

--- cogs/testModal.py
+++ cogs/testModal.py
@@ -1,14 +1,22 @@
-import asyncio
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-@app_commands.command(name="test_modal")
-async def test_modal(interaction: discord.Interaction):
-    class TestModal(discord.ui.Modal, title="Test Modal"):
-        answer = discord.ui.TextInput(label="Say something")
+class TestModal(discord.ui.Modal, title="Test Modal"):
+    answer = discord.ui.TextInput(label="Say something")
 
-        async def on_submit(self, interaction: discord.Interaction):
-            await interaction.response.send_message(f"You said: {self.answer.value}")
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"You said: {self.answer.value}", ephemeral=True)
 
-    await interaction.response.send_modal(TestModal())
+
+class TestModalCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="test_modal", description="Simple modal test")
+    async def test_modal(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(TestModal())
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(TestModalCog(bot))

28f32c5ec691b200c01439521c7d865f1f0d5de5
Mark Vendohr <vondrr@gmail.com>
2025-10-15 15:41:11 +0200

    New additions to the /order_drink command

--- cogs/orderDrink.py
+++ cogs/orderDrink.py
@@ -56,10 +56,10 @@ class OrderDrinkCog(commands.Cog):
         
         embed = discord.Embed(
             title="🍹 Order a Drink",
-            description="Choose your drink by clicking one of the buttons below.\n\n**The Galaxy's Usuals**\n- **🍺 Skannbult Ale** - A cheap beverage, safe to drink.\n- **🍸 Fuzzy Tauntaun** - A **small chance** to get a bonus or debuff to Rolls.\n- **🍷 Whyren’s Reserve** - A **big chance** to get a bonus or debuff to Rolls.\n- **🥛 Bantha Milk** - A **small chance** to get a **BIG bonus** to Rolls, with a **BIG chance** to get a **BIG debuff** to Rolls.",
+            description="Choose your drink by clicking one of the buttons below.\n\n**The Galaxy's Usuals**\n- **🍺 Skannbult Ale** - A cheap beverage, safe to drink.\n- **🥛 Bantha Milk** - A **small chance** to get a bonus or debuff to Rolls.\n- **🍸 Fuzzy Tauntaun** - A **big chance** to get a bonus or debuff to Rolls.\n- **🍷 Whyren’s Reserve** - A **small chance** to get a **BIG bonus** to Rolls, with a **BIG chance** to get a **BIG debuff** to Rolls.",
             color=discord.Color.blurple()
         )
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428000979533434981/DrinkingObiWan.gif")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008654174552147/BarDefault.gif")
 
         view = DrinkButtons()
         await interaction.response.send_message(embed=embed, view=view)
@@ -78,11 +78,11 @@ class DrinkButtons(discord.ui.View):
         # TODO: Take away the credits
 
         embed = discord.Embed(
-            title="🍺 Skannbult Ale",
+            title=f"**{interaction.user.display_name}** ordered the **🍺 Skannbult Ale!**",
             description="You enjoy this cheap beverage!",
             color=discord.Color.light_gray()
         )
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428000979122524172/MilkDrinking.gif")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655772713050/DrinkDefault.gif")
         embed.set_footer(text="It cost 50 ᖬ!")
 
         await interaction.response.send_message(embed=embed)
@@ -97,7 +97,34 @@ class DrinkButtons(discord.ui.View):
         # TODO: implement action
         randomResult = random.randint(1, 10)
 
-        await interaction.response.send_message(f"Bantha Milk! {randomResult}", ephemeral=True)
+        if randomResult >= 1 and randomResult <= 2:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🥛 Bantha Milk!**",
+            description="You are **imbued** with energy after drinking this!\n\nYour Die will have 1 more side for your next Roll!",
+            color=discord.Color.green()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655193641031/DrinkSuccess.gif")
+            embed.set_footer(text="It cost 200 ᖬ!")
+
+        elif randomResult == 3:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🥛 Bantha Milk!**",
+            description="You don't feel so good after drinking it!\n\nYour Die will have 1 **fewer** sides for your next Roll!",
+            color=discord.Color.red()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008654774472704/DrinkWrong.gif")
+            embed.set_footer(text="It cost 200 ᖬ!")
+
+        else:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🥛 Bantha Milk!**",
+            description="You enjoy this beverage!\n\nIt is very good, but doesn't give you any bonuses or debuffs.",
+            color=discord.Color.light_gray()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655772713050/DrinkDefault.gif")
+            embed.set_footer(text="It cost 200 ᖬ!")
+
+        await interaction.response.send_message(embed=embed)
 
     # Button 3
     @discord.ui.button(label="🍸 Fuzzy Tauntaun | 1 000 ᖬ", style=discord.ButtonStyle.green, custom_id="tauntaun")
@@ -109,7 +136,34 @@ class DrinkButtons(discord.ui.View):
         # TODO: implement action
         randomResult = random.randint(1, 10)
 
-        await interaction.response.send_message(f"Fuzzy Tauntaun! {randomResult}", ephemeral=True)
+        if randomResult >= 1 and randomResult <= 3:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍸 Fuzzy Tauntaun!**",
+            description="You are **imbued** with energy after drinking this!\n\nYour Die will have 2 more sides for your next Roll!",
+            color=discord.Color.green()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655193641031/DrinkSuccess.gif")
+            embed.set_footer(text="It cost 1 000 ᖬ!")
+
+        elif randomResult >= 4 and randomResult <= 6:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍸 Fuzzy Tauntaun!**",
+            description="You don't feel so good after drinking it!\n\nYour Die will have 2 **fewer** sides for your next Roll!",
+            color=discord.Color.red()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008654774472704/DrinkWrong.gif")
+            embed.set_footer(text="It cost 1 000 ᖬ!")
+
+        else:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍸 Fuzzy Tauntaun!**",
+            description="You enjoy this expensive beverage!\n\nIt is very good, but doesn't give you any bonuses or debuffs.",
+            color=discord.Color.light_gray()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655772713050/DrinkDefault.gif")
+            embed.set_footer(text="It cost 1 000 ᖬ!")
+
+        await interaction.response.send_message(embed=embed)
 
     # Button 4
     @discord.ui.button(label="🍷 Whyren’s Reserve | 3 000 ᖬ", style=discord.ButtonStyle.red, custom_id="whyren")
@@ -120,8 +174,35 @@ class DrinkButtons(discord.ui.View):
         
         # TODO: implement action
         randomResult = random.randint(1, 10)
+        
+        if randomResult == 1:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍷 Whyren’s Reserve!**",
+            description="You are **imbued** with energy after drinking this!\n\nYour Die will have 3 more sides for your next 2 Rolls!",
+            color=discord.Color.green()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655193641031/DrinkSuccess.gif")
+            embed.set_footer(text="It cost 3 000 ᖬ!")
 
-        await interaction.response.send_message(f"Whyren's Reserve! {randomResult}", ephemeral=True)
+        elif randomResult >= 2 and randomResult <= 8:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍷 Whyren’s Reserve!**",
+            description="You don't feel so good after drinking it!\n\nYour Die will have 3 **fewer** sides for your next 2 Rolls!",
+            color=discord.Color.red()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008654774472704/DrinkWrong.gif")
+            embed.set_footer(text="It cost 3 000 ᖬ!")
+
+        else:
+            embed = discord.Embed(
+            title=f"**{interaction.user.display_name}** ordered the **🍷 Whyren’s Reserve!**",
+            description="You enjoy this exclusive beverage!\n\nIt is very good, but doesn't give you any bonuses or debuffs.",
+            color=discord.Color.light_gray()
+            )
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1422602593179271189/1428008655772713050/DrinkDefault.gif")
+            embed.set_footer(text="It cost 3 000 ᖬ!")
+
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(OrderDrinkCog(bot))
